1a2222a3b396385a9fd5dc4debd87a0b95515da3
Seungju Shin <shinsuengju1@gmail.com>
2023-03-04 13:39:33 +0900

    #7.0 To Do List part One

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
-import { func } from 'prop-types';
-import { useState, useEffect } from 'react';
-
-function Hello() {
-  /*   useEffect(function () {
-    console.log('hi :)');
-    return function () {
-      console.log('bye :(');
-    };
-  }, []); */
-  // 아래는 화살표 함수, 코드가 조금 더 짧음
-  useEffect(() => {
-    console.log('hi :)');
-    return () => console.log('bye :(');
-  }, []);
-  return <h1>Hello</h1>;
-}
-
-// ========= 위와 아래는 아예 같은 기능, 위에가 축약형
-
-/* function Hello() {
-  function byFn() {
-    console.log('bye :(');
-  }
-  function hiFn() {
-    console.log('created :)');
-    return byFn;
-  }
-  useEffect(hiFn, []);
-  return <h1>Hello</h1>;
-} */
+import { useState } from 'react';
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState('');
+  const onChange = (event) => setToDo(event.target.value);
+  const [toDos, setToDos] = useState([]);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDo('');
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type='text'
+          placeholder='Write your to do...'
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
